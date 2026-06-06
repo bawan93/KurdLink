e0ccb3bd1a4c321f95fffd40b6fd4e99c32bbb78
bawan93 <bawanhozhin@outlook.com>
2026-06-06 13:23:50 +0100

    Update page.js

--- app/home/page.js
+++ app/home/page.js
@@ -1,8 +1,9 @@
 'use client'
-export const dynamic = 'force-dynamic'
 import { useState, useEffect } from 'react'
 import { createBrowserClient } from '@supabase/ssr'
 import TX from '../../lib/translations'
+
+export const dynamic = 'force-dynamic'
 
 function getSupabase() {
   return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
